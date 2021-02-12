f6ba0338d884d671906c9e769a34c98496d2431f
Ian Walston <ikwalston@comcast.net>
2021-02-12 17:40:07 -0500

    chat messages scroll

--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -1,40 +1,98 @@
 import React, { useState, useEffect } from 'react'
 import { useRooms } from ".././../hooks"
+import { TextField, Typography, Grid, makeStyles } from "@material-ui/core"
+
+const useStyles = makeStyles(theme => ({
+    input: {
+        background: "red",
+    },
+    messages: {
+        maxHeight: "300px",
+        overflowY: "scroll"
+    },
+    bottom: {
+        height: "20px",
+    },
+    root: {
+        height: "100vh",
+        width: "100vw",
+    }
+}))
 
 function Room() {
     const { getRoomMessages, sendMessage } = useRooms()
     const [messages, setMessages] = useState([])
+    const [userInput, setUserInput] = useState("")
+    const [roomName, setRoomName] = useState("")
+
+    const classes = useStyles()
 
     useEffect(() => {
+        loadMessages()
+
+    }, [])
+    
+    function loadMessages() {
         getRoomMessages().then(res => {
-            console.log(res)
-            setMessages(res.data.messages)
+            setRoomName(res.data.name)
+            setMessages(res.data.messages.reverse())
+            scrollToChatEdge()
         }).catch(err => {
             throw err
         })
-    }, [])
-
-    function handleSendMessage(){
-        sendMessage("react")
     }
 
+    function handleSendMessage() {
+        sendMessage(userInput).then(() => {
+            scrollToChatEdge()
+            setUserInput("")
+            loadMessages()
+        })
+    }
+    function scrollToChatEdge() {
+        document.querySelector('#messages-bottom').scrollIntoView()
+
+    }
+
+    function handleUserInput(e) {
+        setUserInput(e.target.value)
+    }
+
+    function handleUserKeyDown(e) {
+        if (e.key === 'Enter') {
+            handleSendMessage();
+        }
+    }
 
     return (
-        <div>
-            Room
-            
-            {messages.map(message => {
-                return (
-                    <div>
-                        {message.text}
-                    </div>
-                )
-            })}
+        <Grid container>
 
-            <button onClick={handleSendMessage}>
-                send
-            </button>
-        </div>
+            <Grid item xs={12}>
+                <Typography>
+                    {roomName}
+                </Typography>
+            </Grid>
+
+
+            <Grid id="messages" item xs={12} className={classes.messages}>
+                {messages.map(message => {
+                    return (
+                        <div>
+                            {message.text}
+                        </div>
+                    )
+                })}
+                <div id="messages-bottom" className={classes.bottom}></div>
+            </Grid>
+
+            <div className={classes.input}>
+                <TextField value={userInput} onChange={handleUserInput} onKeyDown={handleUserKeyDown}>
+                </TextField>
+                <button onClick={handleSendMessage}>
+                    send
+                        </button>
+            </div>
+        </Grid>
     )
 }
 

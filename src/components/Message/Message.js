46c4a6c186c26ea35b018a283ec36dc955c76611
Alexa Matei <alexa.matei.dev@icloud.com>
2021-02-20 13:54:55 -0500

    new layout + default user images

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,13 +1,34 @@
+/* eslint-disable max-len */
 /* eslint-disable prettier/prettier */
 import React from "react";
-import { Grid, Typography, Box, Button } from "@material-ui/core";
+import { Grid, Typography, Box, Button, makeStyles } from "@material-ui/core";
 import { useTime, useRooms } from "../../hooks";
 //import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
 import ReactTextFormat from "react-text-format";
 import EmojiPicker from "../EmojiPicker/EmojiPicker"
 
+const useStyles = makeStyles((theme) => ({
+  row: {
+    position: "relative",
+    "&:hover > *": {
+      visibility: "visible !important",
+    },
+  },
+  overlay: {
+    visibility: "hidden",
+    position: "absolute",
+    top: theme.spacing(1),
+    right: theme.spacing(1.5),
+  },
+  profilepic: {
+    background: "red",
+    borderRadius: "50%"
+  },
+}));
+
 function Message({ message, showDate }) {
   const { formatDate } = useTime();
+  const classes = useStyles();
   const { createReaction } = useRooms();
 
   function checkURL(url) {
@@ -21,16 +42,32 @@ function Message({ message, showDate }) {
   }
 
   return (
-    <Grid container justify="space-between">
-      <Grid item>
+    <Grid container className={classes.row}>
+      {
+        showDate &&
+        <Grid item container xs={12} justify="space-between">
+          <Box mt={1.5}>
+            <Typography>
+              <Box borderRadius="5%" component="span">
+                <img src="https://micron-web.herokuapp.com/images/micron.png" width="30px" className={classes.profilepic}/>
+              </Box>
+              {message.author} {" "}
+              <Typography variant="caption" color="textSecondary"> {showDate && formatDate(message.created_at)}</Typography>
+            </Typography>
+          </Box>
+        </Grid>
+      }
+      
+      <Grid item xs={12}>
         <Typography variant="body2">
-          {message.author}:{" "}
           {checkURL(message.text) ? (
             <img width="100px" src={message.text} />
           ) : (
             <ReactTextFormat>{message.text}</ReactTextFormat>
           )}
         </Typography>
+      </Grid>
+      <Grid item xs={12}>
         <Box>
           {
             message.reactions.map(reaction => (
@@ -39,10 +76,8 @@ function Message({ message, showDate }) {
           }
         </Box>
       </Grid>
-      <Grid item>
-        <Typography>{showDate && formatDate(message.created_at)}
-          <EmojiPicker onSelect = {handleReaction}></EmojiPicker>
-        </Typography>
+      <Grid item xs={12} className={classes.overlay}>
+        <EmojiPicker onSelect = {handleReaction}></EmojiPicker>
       </Grid>
     </Grid>
   );

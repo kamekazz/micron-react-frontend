f73965e831b5f26c5f0303a50f4ddbfd40993eb0
Alexa Matei <alexa.matei.dev@icloud.com>
2021-02-24 12:50:39 -0500

    added animations in Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,15 @@
 import { Grid, Typography, Box } from "@material-ui/core";
 import React from "react";
 import RoomList from "../RoomList/RoomList";
-import Room from "../Room/Room";
+import { fadeIn } from "react-animations";
+import Radium, {StyleRoot} from "radium";
+
+const styles = {
+  bounce: {
+    animation: "x 2s",
+    animationName: Radium.keyframes(fadeIn, "bounce")
+  }
+}
 
 function Home() {
   function getUsername() {
@@ -11,19 +19,25 @@ function Home() {
   }
 
   return (
-    <Grid container alignItems="stretch">
-      <Grid item xs={12}>
-        <Box display="flex" alignItems="center" justifyContent="center" margin="auto">
-          <Typography variant="h3" align="center">
-            Hello, {getUsername()}!
-          </Typography>
-        </Box>
-        <Box>
-          <Typography variant="h4" align="center">Select a room and start chatting.</Typography>
-          <RoomList />
-        </Box>
-      </Grid>
-    </Grid>
+    <StyleRoot>
+      <div className="test" style={styles.bounce}>
+        <Grid container alignItems="stretch" className="test" style={styles.bounce}>
+          <Grid item xs={12}>
+            <Box display="flex" alignItems="center" justifyContent="center" margin="auto">
+              <Typography variant="h3" align="center">
+                Hello, {getUsername()}!
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h4" align="center">
+                Select a room and start chatting.
+              </Typography>
+              <RoomList />
+            </Box>
+          </Grid>
+        </Grid>
+      </div>
+    </StyleRoot>
   );
 }
 
